3ec7c4c2a3237a9cf946488a2d3580338928ff29
CatCode23 <andrewcatalano23@gmail.com>
2024-11-25 13:25:41 -0500

    added basic crud operations for the workout_data table. Can start testing on front-end component now.

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,112 @@ app.get('/workouts', (request, response) => {
         });
 });
 
+app.get('/workouts/:id', (request, response) => {
+    knex('workout_data')
+        .select('*')
+        .where('id', request.params.id)  
+        .then(data => {
+            response.json(data);  
+        })
+        .catch(err => {
+            console.error(err);
+            response.status(500).json({
+                message: 'An error occurred while fetching the workout data.',
+            });
+        });
+});
+
+app.post('/workouts', (request, response) => {
+    const {
+        activity_day,
+        workout_type,
+        distance,
+        time,
+        calories,
+        total_steps,
+        avg_speed,
+        avg_cadence,
+        max_cadence,
+        avg_pace,
+        max_pace,
+        min_pace,
+        avg_heart_rate,
+        max_heart_rate,
+        min_heart_rate,
+        vo2_max,
+        aerobic,
+        anaerobic,
+        intensive,
+        light
+    } = request.body;
+
+    const workoutData = {
+        activity_day: activity_day || null,
+        workout_type: workout_type || null,
+        distance: distance || null,
+        time: time || null,
+        calories: calories || null,
+        total_steps: total_steps || null,
+        avg_speed: avg_speed || null,
+        avg_cadence: avg_cadence || null,
+        max_cadence: max_cadence || null,
+        avg_pace: avg_pace || null,
+        max_pace: max_pace || null,
+        min_pace: min_pace || null,
+        avg_heart_rate: avg_heart_rate || null,
+        max_heart_rate: max_heart_rate || null,
+        min_heart_rate: min_heart_rate || null,
+        vo2_max: vo2_max || null,
+        aerobic: aerobic || null,
+        anaerobic: anaerobic || null,
+        intensive: intensive || null,
+        light: light || null
+    };
+    knex('workout_data')
+        .insert(workoutData)
+        .returning('*')
+        .then(data => {
+            response.status(201).json({
+                message: 'Workout entry created successfully.',
+                workout: data[0], 
+            });
+        })
+        .catch(err => {
+            console.error(err);
+            response.status(500).json({
+                message: 'Entry could not be created.',
+            });
+        });
+});
+
+app.delete('/workouts/:id', (request, response) => {
+    knex('workout_data')
+        .where('id', request.params.id)
+        .del()
+        .returning('*')
+        .then(deletedWorkout => response.json({
+            message: `Workout entry with id ${request.params.id} deleted successfully.`,
+            deletedWorkout: deletedWorkout[0], 
+        }))
+        .catch(() => response.status(500).json({ 
+            message: 'workout could not be deleted or does not exist.' 
+         })
+    );
+ });
+
+ app.patch('/workouts/:id', (request, response) => {
+    knex('workout_data')
+        .where('id', request.params.id)
+        .update(request.body)
+        .returning('*')
+        .then(updatedWorkout => response.json(updatedWorkout))
+        .catch((err) => response.status(500).json({ 
+            message: 'Requested workout does not exist' 
+        })
+    );
+ });
+
+
 app.listen(port, () => {
     console.log('Your Knex and Express app are running successfully');
 });
